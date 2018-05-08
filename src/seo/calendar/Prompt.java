b7ffc8c1683a9d7bf6fce05207f1e91d4732ae55
kendoseo <kendoseo@naver.com>
2018-05-08 14:42:08 +0900

    Remove String if-else

--- src/seo/calendar/Prompt.java
+++ src/seo/calendar/Prompt.java
@@ -21,22 +21,24 @@ public class Prompt {
 	 * @return 0 ~ 6 (0= Sunday, 6=Saturday)
 	 */
 	public int parseDay(String week) {
-		if (week.equals("su")) {
+		switch (week) {
+		case "su":
 			return 0;
-		} else if (week.equals("mo")) {
+		case "mo":
 			return 1;
-		} else if (week.equals("tu")) {
+		case "tu":
 			return 2;
-		} else if (week.equals("we")) {
+		case "we":
 			return 3;
-		} else if (week.equals("th")) {
+		case "th":
 			return 4;
-		} else if (week.equals("fr")) {
+		case "fr":
 			return 5;
-		} else if (week.equals("sa")) {
+		case "sa":
 			return 6;
-		} else
+		default:
 			return 0;
+		}
 	}
 
 	public void runPrompt() throws ParseException {
@@ -45,20 +47,27 @@ public class Prompt {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		while (true) {
+		boolean isLoop = true;
+		while (isLoop) {
 			System.out.println("명령 (1, 2, 3, h, q)");
 			String cmd = scanner.next();
-			if (cmd.equals("1")) {
+			switch(cmd){
+			case "1":
 				cmdRegister(scanner, cal);
-			} else if (cmd.equals("2")) {
-				cmdSearch(scanner, cal);
-			} else if (cmd.equals("3")) {
-				cmdCal(scanner, cal);
-			} else if (cmd.equals("h")) {
-				printMenu();
-			} else if (cmd.equals("q"))
 				break;
-
+			case "2":
+				cmdSearch(scanner, cal);
+				break;
+			case "3":
+				cmdCal(scanner, cal);
+				break;
+			case "h":
+				printMenu();
+				break;
+			case "q":
+				isLoop =false;
+				break;
+			}
 		}
 
 		System.out.println("Thank you, Bye~");
@@ -104,10 +113,10 @@ public class Prompt {
 		String date = s.next();
 		String text = "";
 		System.out.println("일정을 입력해주세요. (문장의 끝에 ;을 입력해주세요.)");
-		while (true){
+		while (true) {
 			String word = s.next();
-			text += word +" ";
-			if (word.endsWith(";")){
+			text += word + " ";
+			if (word.endsWith(";")) {
 				break;
 			}
 		}
